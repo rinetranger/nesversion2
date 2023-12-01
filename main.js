0d05af0ef34ae9c02efe147d8b734d15c50e707c
kishirinichiro <rkishi@samty.co.jp>
2023-12-01 18:02:45 +0900

    20231201

--- main.js
+++ main.js
@@ -1,101 +1,64 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const menuIcon = document.getElementById("menuIcon");
-    const x01Icon = document.querySelector(".frame-child44");
-    const frameRoot = document.querySelector(".frame-root");
+/*js-1 ハンバーガーメニュ開く閉じる */
+var trigger = document.querySelector('.openbtn4');
+const humbergerinner = document.getElementById("humberger-inner");
 
-    menuIcon.addEventListener("click", function() {
-      frameRoot.style.display = "block";
-      
-    });
-
-    x01Icon.addEventListener("click", function() {
-      frameRoot.style.display = "none";
-    });
-  });
-
-
-  document.getElementById('downloadPdfButton').addEventListener('click', function() {
-    // ここにPDFファイルのURLを指定する
-    var pdfUrl = './NESTA_Map.pdf';
+function hamburger() {
+  trigger.classList.toggle('active');
   
-    // PDFファイルを新しいタブで開く
-    window.open(pdfUrl, '_blank');
-  });
-
-
-
-  const container = document.querySelector('.ticket-item-container');
-  const items = document.querySelectorAll('.ticket-item');
-  let currentIndex = 1; // 2番目のアイテムから開始
-  let autoSlideInterval;
-  let isDragging = false;
-  let startPos = 0;
-  let currentTranslate = 0;
-  let prevTranslate = 0;
-  
-  function setContainerTransition(enabled) {
-    container.style.transition = enabled ? 'transform 0.3s ease-out' : '';
+  // 画面の幅をチェック
+  if (window.innerWidth <= 768) { // 768px以下の場合はモバイルとみなす
+    humbergerinner.classList.toggle('in'); // モバイル用のクラス
+  } else {
+    humbergerinner.classList.toggle('in2'); // それ以外の場合のクラス
   }
-  
-  function setPositionByIndex() {
-    currentTranslate = currentIndex * -window.innerWidth;
-    setContainerTransition(true);
-    container.style.transform = `translateX(${currentTranslate}px)`;
-  }
-  
-  function startAutoSlide() {
-    autoSlideInterval = setInterval(() => {
-      currentIndex = (currentIndex + 1) % items.length;
-      setPositionByIndex();
-    }, 3000);
-  }
-  
-  function touchStart(index, event) {
-    currentIndex = index;
-    startPos = getPositionX(event);
-    isDragging = true;
-    setContainerTransition(false);
-  }
-  
-  function touchMove(event) {
-    if (isDragging) {
-      const currentPosition = getPositionX(event);
-      currentTranslate = prevTranslate + currentPosition - startPos;
-      container.style.transform = `translateX(${currentTranslate}px)`;
+}
+
+trigger.addEventListener('click', function() {
+  hamburger();
+});
+
+/*js-2 ヘッダースクロール透過 */
+  window.addEventListener('scroll', function() {
+
+    
+    var header = document.querySelector('.header');
+    var scrollPosition = window.scrollY || document.documentElement.scrollTop;
+    console.log(scrollPosition);
+    if (scrollPosition > 100) { // 100pxスクロールしたら変化を開始
+        header.style.backgroundColor = 'rgba(255, 255, 255, 0.3)'; // 半透明に
+    } else {
+        header.style.backgroundColor = 'rgba(255, 255, 255, 1)'; // 完全な白色に
     }
-  }
-  
-  function touchEnd() {
-    isDragging = false;
-    const movedBy = currentTranslate - prevTranslate;
-  
-    // スワイプの距離が50pxを超えたら次または前のアイテムに移動
-    if (movedBy < -50 && currentIndex < items.length - 1) currentIndex += 1;
-    if (movedBy > 50 && currentIndex > 0) currentIndex -= 1;
-  
-    setPositionByIndex();
-    prevTranslate = currentTranslate;
-  
-    // 自動スライドを再開
-    startAutoSlide();
-  }
-  
-  function getPositionX(event) {
-    return event.type.includes('mouse') ? event.pageX : event.touches[0].clientX;
-  }
-  
-  // イベントリスナーを設定
-  items.forEach((item, index) => {
-    const itemContainer = item;
-    itemContainer.addEventListener('mousedown', touchStart.bind(this, index));
-    itemContainer.addEventListener('touchstart', touchStart.bind(this, index));
-  });
-  
-  window.addEventListener('mousemove', touchMove);
-  window.addEventListener('touchmove', touchMove);
-  window.addEventListener('mouseup', touchEnd);
-  window.addEventListener('touchend', touchEnd);
-  
-  // カルーセルの自動スライドを開始
-  startAutoSlide();
-  
+});
+
+
+/*js-3 横スクロール */
+// document.addEventListener("DOMContentLoaded", function() {
+//   const ticketItemContainer = document.querySelector('.ticket-item-container');
+//   // アイテムの複製
+//   Array.from(ticketItemContainer.children).forEach(item => {
+//     const clone = item.cloneNode(true);
+//     ticketItemContainer.appendChild(clone);
+//   });
+
+//   let scrollAmount = 0;
+//   const speed = 0.5; // スクロール速度（調整可能）
+
+//   function moveItems() {
+//     scrollAmount += speed;
+//     ticketItemContainer.style.transform = `translateX(-${scrollAmount}px)`;
+
+//     // スクロールが特定の幅を超えたらリセット
+//     const totalWidth = ticketItemContainer.scrollWidth;
+//     if (scrollAmount >= totalWidth / 2) {
+//       scrollAmount = 0; // スクロールリセット
+//     }
+
+//     requestAnimationFrame(moveItems);
+//   }
+
+//   moveItems(); // アニメーション開始
+// });
+
+
+
