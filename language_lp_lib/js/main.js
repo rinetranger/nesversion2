8f1b5bc5603a62fbbd23ad84c2cea4d4f7a3b9e9
rinetranger <rinetranger@gmail.com>
2023-12-11 01:30:26 +0900

    20231211v1

--- language_lp_lib/js/main.js
+++ language_lp_lib/js/main.js
@@ -4,33 +4,12 @@ const humbergerinner = document.getElementById("humberger-inner");
 
 function hamburger() {
   trigger.classList.toggle('active');
-  
-  // 画面の幅をチェック
-  if (window.innerWidth <= 768) { // 768px以下の場合はモバイルとみなす
-    humbergerinner.classList.toggle('in'); // モバイル用のクラス
-  } else {
-    humbergerinner.classList.toggle('in2'); // それ以外の場合のクラス
-  }
+  humbergerinner.classList.toggle('in'); // モバイル用のクラス
 }
 
 trigger.addEventListener('click', function() {
   hamburger();
 });
-
-/*js-2 ヘッダースクロール透過 */
-  window.addEventListener('scroll', function() {
-
-    
-    var header = document.querySelector('.header');
-    var scrollPosition = window.scrollY || document.documentElement.scrollTop;
-    console.log(scrollPosition);
-    if (scrollPosition > 100) { // 100pxスクロールしたら変化を開始
-        header.style.backgroundColor = 'rgba(255, 255, 255, 0.3)'; // 半透明に
-    } else {
-        header.style.backgroundColor = 'rgba(255, 255, 255, 1)'; // 完全な白色に
-    }
-});
-
 
 /*js-3 横スクロール */
 document.addEventListener("DOMContentLoaded", function() {
@@ -44,8 +23,6 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log("testdd")
   }
 });
-
-
 
 /*js-4 activity */
 document.addEventListener("DOMContentLoaded", function() {
